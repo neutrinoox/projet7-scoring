50964e7a0ef021823421bebcfd56c8a05d5f632d
neutrinoox <234262114+neutrinoox@users.noreply.github.com>
2025-10-15 23:20:26 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -70,4 +70,6 @@ def predict(client: ClientFeatures):
         raise HTTPException(status_code=400, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
